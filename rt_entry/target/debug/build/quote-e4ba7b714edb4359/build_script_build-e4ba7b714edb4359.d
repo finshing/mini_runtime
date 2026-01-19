2f37bef944f08ef9fbfc7e958ab45499f9a8add5
wangfp <wangfp_nk@163.com>
2026-01-19 07:12:56 -0800

    change name

--- rt_entry/target/debug/build/quote-e4ba7b714edb4359/build_script_build-e4ba7b714edb4359.d
+++
@@ -1,5 +0,0 @@
-/home/wang/codes/mini_runtime/rt_entry/target/debug/build/quote-e4ba7b714edb4359/build_script_build-e4ba7b714edb4359.d: /home/wang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-e791a3f93f26854f/quote-1.0.43/build.rs
-
-/home/wang/codes/mini_runtime/rt_entry/target/debug/build/quote-e4ba7b714edb4359/build_script_build-e4ba7b714edb4359: /home/wang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-e791a3f93f26854f/quote-1.0.43/build.rs
-
-/home/wang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-e791a3f93f26854f/quote-1.0.43/build.rs:
--- rt_entry/target/debug/deps/rt_entry-6892d406dcec906c.d
+++
@@ -1,9 +0,0 @@
-/home/wang/codes/mini_runtime/rt_entry/target/debug/deps/rt_entry-6892d406dcec906c.d: src/lib.rs Cargo.toml
-
-/home/wang/codes/mini_runtime/rt_entry/target/debug/deps/librt_entry-6892d406dcec906c.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
